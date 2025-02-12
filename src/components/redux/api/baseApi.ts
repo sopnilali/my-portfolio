2a54cc5fd63d129bda1ae5f76f699f73221cd4f6
sopnilali <sopnilali24@gmail.com>
2025-02-13 01:58:25 +0600

    added

--- src/components/redux/api/baseApi.ts
+++ src/components/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v2',
+    baseUrl: 'https://my-portfolio-server-amber.vercel.app/api/v2',
     credentials: 'include',
   }),
 
